94576b3f01e850ecfcedcbaa45ca8aa9550b7aab
lusparon332 <lusparon222@gmail.com>
2023-12-02 19:24:40 +0300

    add card prefab

---
+++ Assets/Scripts/Card Instant Script.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInstantScript : CardBaseScript
+{
+    public int attack = 0;
+    public int defense = 0;
+    public int gettingMana = 0;
+
+    public override void CardActivate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Card Playable Script.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlayableScript : CardBaseScript
+{
+    public int attack = 0;
+    public int defense = 0;
+    public int spendMana = 0;
+
+    public override void CardActivate()
+    {
+
+    }
+}
