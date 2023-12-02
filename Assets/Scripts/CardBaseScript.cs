934c444ce786f6c4c186a0b9933775b414326e4a
NikitaBogoslovskiy <nbog@sfedu.ru>
2023-12-02 21:31:33 +0300

    add card drag-and-drop

---
+++ Assets/Scripts/CardBaseScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardBaseScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private Vector2 startPosition;
+    
+    private void Start()
+    {
+        canvas = transform.parent.parent.gameObject.GetComponent<Canvas>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) 
+    {
+        startPosition = rectTransform.anchoredPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) 
+    {
+        var cardBottom = rectTransform.anchoredPosition.y - rectTransform.rect.height / 2;
+        if (cardBottom > 0)
+        {
+            CardActivate();
+            Destroy(gameObject);
+        }
+        else
+        {
+            rectTransform.anchoredPosition = startPosition;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) { }
+
+    public virtual void CardActivate()
+    {
+        Debug.Log("Wow");
+    }
+}
--- Assets/Scripts/CardInstantScript.cs
+++ Assets/Scripts/CardInstantScript.cs
@@ -10,6 +10,6 @@ public class CardInstantScript : CardBaseScript
 
     public override void CardActivate()
     {
-        
+
     }
 }
