e5521f9b1e710d707148f5bded2c9888bffebdfe
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2024-05-25 00:12:27 +0300

    Added emerald city road decorations

--- Assets/Scripts/Decorations/Decorations.cs
+++ Assets/Scripts/Decorations/Decorations.cs
@@ -1,6 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Decorations : MonoBehaviour
 {
+    protected static readonly System.Random random = new();
     public virtual void Decorate() { }
+
+    protected static Sprite RandomSprite(List<Sprite> sprites)
+    {
+        return sprites[random.Next(0, sprites.Count)];
+    }
 }
