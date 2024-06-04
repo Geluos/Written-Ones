a06c2e59db4a4484529f24673ad14e538b51bcad
NikitaBogoslovskiy <nbog@sfedu.ru>
2024-06-04 20:20:08 +0300

    add layout for stats

---
+++ Assets/Scripts/UI/HealthBarStat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarStat : MonoBehaviour
+{
+    public Slider slider;
+    public Image fill;
+	public Character character;
+	public TMPro.TextMeshProUGUI hpValueText;
+
+    private void OnEnable()
+    {
+        SetHealth();
+    }
+
+    private void Update()
+    {
+        SetHealth();
+    }
+
+    public void SetHealth()
+    {
+		if (character!=null)
+		{
+			if (slider.value != character.current_hp)
+			{
+				slider.value = character.current_hp;
+			}
+			hpValueText.text = character.current_hp.ToString();
+		} 
+    }
+}
